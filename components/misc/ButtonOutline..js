3911879e91ad933ba3bb48e44cd9f74397645e75
Junjie Guan <gjj2684@gmail.com>
2024-06-29 18:15:39 -0700

    change shawdow color too

--- components/misc/ButtonOutline..js
+++ components/misc/ButtonOutline..js
@@ -1,6 +1,6 @@
 const ButtonOutline = ({ children }) => {
   return (
-    <button className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-blue-200 text-blue-200 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-blue-200 hover:text-white-500 transition-all hover:shadow-orange ">
+    <button className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-blue-200 text-blue-200 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-blue-200 hover:text-white-500 transition-all hover:shadow-blue ">
       {" "}
       {children}
     </button>
--- components/misc/ButtonPrimary.js
+++ components/misc/ButtonPrimary.js
@@ -2,7 +2,7 @@ const ButtonPrimary = ({ children, addClass }) => {
   return (
     <button
       className={
-        "py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-blue-200 hover:shadow-orange-md transition-all outline-none " +
+        "py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-blue-200 hover:shadow-blue-md transition-all outline-none " +
         addClass
       }
     >
